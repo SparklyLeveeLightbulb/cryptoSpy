b52c680e5224c233e283181973e46b14ebb0242b
Nicole Creshon <ncreshon@gmail.com>
2017-12-26 20:20:32 -0600

    Add user and coin tables and create database

---
+++ config/database.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize({
+  username: 'root',
+  password: '',
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Agent = sequelize.define('agent', {
+  id_agent: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  agentName: { type: Sequelize.STRING },
+  password: { type: Sequelize.STRING },
+  phoneNumber: { type: Sequelize.INTEGER}
+
+});
+const Coin = sequelize.define('coin', {
+  id_coin: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  },
+  coinName: { type: Sequelize.STRING },
+  currentValue: { type: Sequelize.INTEGER},
+  lastValue: { type: Sequelize.INTEGER}
+
+});
