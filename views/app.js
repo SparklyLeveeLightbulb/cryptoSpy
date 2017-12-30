8d73c806dee1bb50c0a9280c219b3f52fc27f4b6
Nicole Creshon <ncreshon@gmail.com>
2017-12-29 18:01:42 -0600

    profile is rendering and dynamic

--- views/app.js
+++ views/app.js
@@ -1,10 +1,34 @@
-const app = angular.module('cryptoProfile', []);
+const app = angular.module('login')
+app.component('cryptoProfile', {
+  templateUrl: 'profile.html',
+  bindings: {
+   
+  }
+});
 
-// angular.module('cryptoProfile');
+app.factory('appFactory', ($http) => ({
+  getOnInit: (callback) => {
+  $http({
+  method: 'GET',
+    url: '/getProfile'
+  }).then(function successCallback(response) {
+    console.log(response)
+    callback(response.data)
+  }, function errorCallback(response) {
+    console.log(response)
+  })
+}
+    
 
-app.controller('cryptoCtrl',($scope) => {
-$scope.text = 'hey'
+
+}));
+
+app.controller('cryptoCtrl',($scope, appFactory) => {
+
+  $scope.coins = [{ coinName: 'Bitcoin', currentValue: 14000, amountOwned: 2 }, { coinName: 'Ethereum', currentValue: 5000, amountOwned: 5 }, { coinName: 'Bitcoin', currentValue: 14000, amountOwned: 2 }];
 $scope.submit = () => {
-  console.log($scope.text)
+  appFactory.getOnInit((response) => {
+    $scope.coins = response;
+  })
 }
 });
